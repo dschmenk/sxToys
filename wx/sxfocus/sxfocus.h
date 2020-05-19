a838c13b329575c8d990545348c776ae97ba25b1
David Schmenk <dschmenk@gmail.com>
2020-05-19 14:40:34 -0700

    Try to get retina resolution for bundle - no dice yet

--- wx/sxfocus/sxfocus.h
+++ wx/sxfocus/sxfocus.h
@@ -1,7 +1,7 @@
 #include "sxccd.h"
 #if _MSC_VER < 1700
-typedef  unsigned char uint8_t;
-typedef  signed    char int8_t;
-typedef  unsigned short uint16_t;
-typedef  signed    short int16_t;
+typedef unsigned char  uint8_t;
+typedef signed   char  int8_t;
+typedef unsigned short uint16_t;
+typedef signed   short int16_t;
 #endif
