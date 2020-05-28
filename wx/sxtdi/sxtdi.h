c2819a074295ce1000c55a14f77c8579f2bee1d7
David Schmenk <dschmenk@sbcglobal.net>
2020-05-27 19:24:25 -0700

    Must have deleted character by accident

--- wx/sxtdi/sxtdi.h
+++ wx/sxtdi/sxtdi.h
@@ -8,7 +8,7 @@ typedef unsigned short uint16_t;
 typedef signed   short int16_t;
 #endif
 #include <timeapi.h>
-define ENABLE_HIGH_RES_TIMER() if (tdiExposure < 1000) timeBeginPeriod(1)
+#define ENABLE_HIGH_RES_TIMER() if (tdiExposure < 1000) timeBeginPeriod(1)
 #define DISABLE_HIGH_RES_TIMER() if (tdiExposure < 1000) timeEndPeriod(1)
 #else
 #include <sys/time.h>
