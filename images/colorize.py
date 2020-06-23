e0d8e315a7e6113b7a0435334557b5ecfb74794a
David Schmenk <dschmenk@gmail.com>
2020-06-22 20:14:47 -0700

    Clean up colorize

--- images/colorize.py
+++ images/colorize.py
@@ -9,7 +9,7 @@ from astropy.io import fits
 def fill_masked_frame(masked_frame) :
     pad_frame = np.pad(masked_frame, 1, 'reflect')
     vfill     = pad_frame[0:-1] + pad_frame[1:]
-    hfill      = vfill[:,0:-1]  + vfill[:,1:]
+    hfill     = vfill[:,0:-1]   + vfill[:,1:]
     return hfill[:-1,:-1]
 
 if len(argv) < 2 :
@@ -40,7 +40,7 @@ magenta_mask = np.array([[0, 0],
                          [0, 1]])
 magenta_frame = fill_masked_frame(raw_frame * np.tile(magenta_mask, (int(580/4),int(752/2)))) / 65536.0
 #
-# Calculateluminance frame
+# Calculate YUV and RGB frames
 #
 y_frame = magenta_frame * 0.05 + cyan_frame * 0.5 + yellow_frame * 0.25 + green_frame * 0.3
 u_frame = magenta_frame + cyan_frame - green_frame - yellow_frame
@@ -48,22 +48,9 @@ v_frame = magenta_frame + yellow_frame - green_frame - cyan_frame
 r_frame = y_frame                    + 1.14  * v_frame
 g_frame = y_frame - 0.345  * u_frame - 0.581 * v_frame
 b_frame = y_frame + 0.2032 * u_frame
-# Reduce image based on median (background) level: reduce_pix = (1 - image_median)/(1 - column_median)*(raw_pix - column_median) + image_median
-#scan_med = np.median(fscan)
-#for col in range(columns) :
-#    col_med = np.median(fscan[:,col])
-#    fscan[:,col] -= col_med
-#    fscan[:,col] *= (1-scan_med)/(1-col_med)
-#    fscan[:,col] += scan_med
 #
-#if len(argv) < 3 :
-#    reduce_scan = 'reduce_scan.fits'
-#else :
-#    reduce_scan = argv[2]
-#fscan_hdu = fits.PrimaryHDU(fscan)
-#fscan_hdu.writeto(reduce_scan)
-
 # Plot the result
+#
 fig = plt.figure()
 #plt.imshow(r_frame)
 #fig = plt.figure()
