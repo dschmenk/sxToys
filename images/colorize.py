f74fced235c1116c7561477321343f08f519c044
David Schmenk <dschmenk@gmail.com>
2020-06-24 17:27:04 -0700

    YUV->RGB WIP

--- images/colorize.py
+++ images/colorize.py
@@ -42,17 +42,20 @@ magenta_frame = fill_masked_frame(raw_frame * np.tile(magenta_mask, (int(580/4),
 #
 # Calculate YUV and RGB frames
 #
-m_coeff = 0.05#0.15
-g_coeff = 0.4#0.19
-c_coeff = 0.1#0.19
-y_coeff = 0.05#0.14
+m_coeff = 0.05#0.15#
+g_coeff = 0.3#0.19#
+c_coeff = 0.05#0.19#
+y_coeff = 0.2#0.14#
 l_coeff = np.array([[m_coeff, g_coeff],
                     [c_coeff, y_coeff],
                     [g_coeff, m_coeff],
                     [c_coeff, y_coeff]])
-#y_frame = fill_masked_frame(raw_frame * np.tile(l_coeff, (int(580/4),int(752/2)))) / 65535.0
-y_frame = (magenta_frame * 0.05 + cyan_frame * 0.05 + yellow_frame * 0.25 + green_frame * 0.3)
-#y_frame = (magenta_frame * 0.1 + cyan_frame * 0.2 + yellow_frame * 0.2 + green_frame * 0.2)
+y_frame = fill_masked_frame(raw_frame * np.tile(l_coeff, (int(580/4),int(752/2)))) / 65535.0
+#magenta_frame *= 0.5
+#cyan_frame    *= 0.9
+#yellow_frame  *= 0.5
+#green_frame   *= 1.0
+#y_frame = magenta_frame * m_coeff + cyan_frame * c_coeff + yellow_frame *y_coeff + green_frame * g_coeff
 u_frame = (magenta_frame + cyan_frame   - green_frame - yellow_frame) * 0.5
 v_frame = (magenta_frame + yellow_frame - green_frame - cyan_frame)   * 0.5
 r_frame = y_frame                   + 1.14  * v_frame
@@ -70,5 +73,5 @@ print ("Max B = ", np.max(b_frame), "Min B = ", np.min(b_frame))
 #plt.figure()
 #plt.imshow(raw_frame, origin="lower")
 ax = RGBAxes(plt.figure(), [0.0, 0.0, 1.0, 1.0])
-ax.imshow_rgb(r_frame, g_frame, b_frame, origin="lower", interpolation="nearest")
+ax.imshow_rgb(r_frame, g_frame * 1.5, b_frame, origin="lower", interpolation="nearest")
 plt.show()
